37285cd5974cb9ca962b29794abd650be3d1bfb4
jbfrericks <60147704+jbfrericks6@users.noreply.github.com>
2023-09-02 12:53:54 -0400

    idk

--- project2/Assets/Scripts/SpawnedObject.cs
+++ project2/Assets/Scripts/SpawnedObject.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 public class SpawnedObject : MonoBehaviour
 {
     float lifeTme = 10;
-    
+    public Material[] materials;
+    public Renderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        MeshRenderer myRenderer = GetComponent<MeshRenderer>();
-        if (myRenderer != null)
+        myRenderer = GetComponent<Renderer>();
+        if (materials.Length > 0)
         {
-            Material myMaterial = myRenderer.material;
+            myRenderer.material = materials[0];
         }
     }
 
@@ -26,7 +27,29 @@ public class SpawnedObject : MonoBehaviour
             
         }
     }
+    public void ChangeMaterial()
+    {
+        // If no materials assigned or only one, just return
+        if (materials.Length <= 1)
+        {
+            Debug.LogWarning("Insufficient materials assigned to switch.");
+            return;
+        }
 
+        // Find the current material's index
+        int currentIndex = System.Array.IndexOf(materials, myRenderer.material);
+
+        // If the material is not in the list or it's the last one, set to the first material
+        if (currentIndex == -1 || currentIndex == materials.Length - 1)
+        {
+            myRenderer.material = materials[1];
+        }
+        else
+        {
+            // Set to the next material
+            myRenderer.material = materials[currentIndex + 1];
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         //GameObject.Destroy(gameObject);
@@ -34,7 +57,8 @@ public class SpawnedObject : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        GameObject.Destroy(gameObject);
+        //GameObject.Destroy(gameObject);
+        ChangeMaterial();
     }
 
     private void OnTriggerStay(Collider other)
