663b85b1569928dfd5aa9726c8f1ca422bdb1ca8
jbfrericks <60147704+jbfrericks6@users.noreply.github.com>
2023-09-08 13:08:17 -0400

    size, scale, color

---
+++ project2/Assets/SizeChanger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeChanger : MonoBehaviour
+{
+    public Slider sizeSlider;  
+    public Transform objectToResize;
+    public GameObject cube;
+    private Vector3 initialScale; 
+
+    void Start()
+    {
+        initialScale = objectToResize.localScale; 
+
+        sizeSlider.onValueChanged.AddListener(OnSliderValueChanged);  
+
+        //initialScale = cube.GetComponent<Vector3>();
+    }
+
+    void OnSliderValueChanged(float value)
+    {
+        objectToResize.localScale = initialScale * value; 
+    }
+}
