db4389eacaade83c25083a1d7e54ddab42ecb051
jbfrericks <60147704+jbfrericks6@users.noreply.github.com>
2023-11-06 11:55:13 -0500

    painting idk
    Former-commit-id: e9ff0d1c446ed37331efad04a24ca8abefb46ee2

--- VRShooter/Assets/Scripts/GunShooting.cs
+++ VRShooter/Assets/Scripts/GunShooting.cs
@@ -2,41 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VelUtils;
+using VelUtils.VRInteraction;
 
 public class GunShooting : MonoBehaviour
 {
-    public GameObject projectilePrefab; // The prefab of the object you want to shoot
-    public Transform shootingPoint; // The point from which the projectiles will be instantiated
-    public float shootingForce = 10f; // The force applied to the projectile to make it move
+    public GameObject projectilePrefab; 
+    public Transform shootingPoint;
+    public float shootingForce; 
     public Side side = Side.Right;
+    private VRMoveable moveable;
+
+    private void Start()
+    {
+        moveable = GetComponent<VRMoveable>();
+    }
     // Update is called once per frame
     void Update()
     {
-        // Check for input
-        if (Input.GetButtonDown("Fire1")) // This assumes you have an input set up named "Fire1"
+        if (moveable.GrabbedBy != null)
         {
-            Shoot();
-        }
-        if (InputMan.Button2Down(side))
-        {
-            Shoot();
+
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+            if (InputMan.Button2Down(side))
+            {
+                Shoot();
+            }
         }
     }
 
     void Shoot()
     {
-        // Instantiate the projectile
         GameObject projectile = Instantiate(projectilePrefab, shootingPoint.position, shootingPoint.rotation);
-
-        // Apply force to the projectile to make it move
         Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
-        if (projectileRb != null)
-        {
-            projectileRb.AddForce(shootingPoint.forward * shootingForce, ForceMode.Impulse);
-        }
-        else
-        {
-            Debug.LogError("Projectile does not have a Rigidbody component");
-        }
+        projectileRb.AddForce(shootingPoint.forward * shootingForce, ForceMode.Impulse);
+
     }
 }
